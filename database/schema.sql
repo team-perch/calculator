ade2f827979613554b938f401c1a0fefc547930c
Joshua Lee <joshua_lee@college.harvard.edu>
2019-12-19 19:31:41 -0800

    postgreSQL schema design

--- database/schema.sql
+++ database/schema.sql
@@ -1,16 +1,29 @@
+CREATE DATABASE name
+    [ [ WITH ] [ OWNER [=] user_name ]
+           [ TEMPLATE [=] template ]
+           [ ENCODING [=] encoding ]
+           [ LC_COLLATE [=] lc_collate ]
+           [ LC_CTYPE [=] lc_ctype ]
+           [ TABLESPACE [=] tablespace ]
+           [ CONNECTION LIMIT [=] connlimit ] ]
+            \connect --databasename
+
 CREATE TABLE IF NOT EXISTS zips(
   zip_code VARCHAR(10) NOT NULL,
   property_tax_rate DECIMAL(5,3),
+  population_of_zip INTEGER,
+  crime_rate DECIMAL(5,3),
   CONSTRAINT zip_code PRIMARY KEY (zip_code)
 );
 
 CREATE TABLE IF NOT EXISTS properties(
-  property_id INTEGER NOT NULL,
+  property_id SERIAL PRIMARY KEY,
   zip_code VARCHAR(10),
+  square_feet INTEGER,
   redfin_cost_estimate INTEGER,
   insurance_rate DECIMAL(5,3),
+  -- seed without fk constraints and index after seeding database
   FOREIGN KEY (zip_code) REFERENCES zips(zip_code),
-  CONSTRAINT property_id PRIMARY KEY (property_id)
 );
 
 CREATE TABLE IF NOT EXISTS lenders(
